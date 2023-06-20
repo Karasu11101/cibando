dc558b7124c052fd302321da73d88662848e5168
Karasu11101 <corvustacitus@gmail.com>
2023-06-20 22:42:01 +0900

    Second up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cibando';
+  nome = 'Matteo';
+  label = 'logo di cibando';
+
+  allievi = [
+    {nome: 'Matteo',
+  cognome: 'Forcina',
+  eta: 29},
+  {nome: 'Mario',
+  cognome: 'Rossi',
+  eta: 25},
+  {nome: 'Enrico',
+  cognome: 'Bianchi',
+  eta: 45},
+  {nome: 'Carlo',
+  cognome: 'Verdi',
+  eta: 35}
+  ];
+
 }
